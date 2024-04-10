e9c005cb9b11e52e5dc3b365ea973aecd1219aa3
Bononi <53324114+Bonnivaldo@users.noreply.github.com>
2024-04-10 19:34:10 -0300

    Change VerticalPage to LazyColumn

--- app/src/main/java/com/example/shortnews/presentation/views/HomeScreen.kt
+++ app/src/main/java/com/example/shortnews/presentation/views/HomeScreen.kt
@@ -3,62 +3,57 @@ package com.example.shortnews.presentation.views
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.pager.PageSize
-import androidx.compose.foundation.pager.VerticalPager
-import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.shortnews.data.entity.Article
 import com.example.shortnews.presentation.components.Loader
 import com.example.shortnews.presentation.components.NewsRowComponent
 import com.example.shortnews.presentation.viewmodel.NewsViewModel
 import com.example.utils.ResourceState
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
-    viewModel: NewsViewModel = hiltViewModel()
+    viewModel: NewsViewModel = hiltViewModel(),
+    onArticleSelected: (Article) -> Unit
 ) {
     val newsResponse by viewModel.news.collectAsState()
-    val pagerState = rememberPagerState(
-        initialPage = 0,
-        initialPageOffsetFraction = 0f
-    ) {
-        100
-    }
+    val pagerState = rememberLazyListState(
+        initialFirstVisibleItemIndex = 0,
+        initialFirstVisibleItemScrollOffset = 0
+    )
+
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        VerticalPager(
-            state = pagerState,
-            modifier = Modifier.fillMaxSize(),
-            pageSize = PageSize.Fill,
-            pageSpacing = 8.dp
-        ) { page ->
-            when (newsResponse) {
-                is ResourceState.Loading<*> -> {
-                    Loader()
-                    Log.d("HOME_SCREEN","INTO LOADING")
+        when (newsResponse) {
+            is ResourceState.Loading<*> -> {
+                Loader()
+                Log.d("HOME_SCREEN", "INTO LOADING")
+            }
 
-                }
-
-                is ResourceState.Success<*> -> {
-                    val response = (newsResponse as ResourceState.Success).data
-                    Log.d("HOME_SCREEN","INTO SUCCESS")
-                    if (response.articlesList.isNotEmpty()) {
-                        NewsRowComponent(page, response.articlesList[page])
+            is ResourceState.Success<*> -> {
+                val response = (newsResponse as ResourceState.Success).data
+                Log.d("HOME_SCREEN", "INTO SUCCESS")
+                if (response.articles.isNotEmpty()) {
+                    LazyColumn(state = pagerState) {
+                        items(response.totalResults) { index ->
+                            val articles = response.articles[index]
+                            NewsRowComponent(articles, onArticleSelected)
+                        }
                     }
                 }
+            }
 
-                is ResourceState.Error<*> -> {
-                    val error = (newsResponse as ResourceState.Error)
-                    Log.d("HOME_SCREEN","INTO ERROR: $error")
-                }
+            is ResourceState.Error<*> -> {
+                val error = (newsResponse as ResourceState.Error)
+                Log.d("HOME_SCREEN", "INTO ERROR: $error")
             }
         }
     }
@@ -67,5 +62,7 @@ fun HomeScreen(
 @Preview
 @Composable
 fun HomeScreenPreview() {
-//    HomeScreen(NewsViewModel())
+    HomeScreen(onArticleSelected = { article ->
+
+    })
 }
